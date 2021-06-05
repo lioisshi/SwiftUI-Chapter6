518c6330d35b71e9696d8f93193edea1eb4b8818
lioisshi <84262564+lioisshi@users.noreply.github.com>
2021-06-05 13:58:34 +0900

    Initial Commit

---
+++ SwiftUI-Chapter6/SwiftUI_Chapter6App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Chapter6App.swift
+//  SwiftUI-Chapter6
+//
+//  Created by Hiroaki Ishiga on 2021/06/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Chapter6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
